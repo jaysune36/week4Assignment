af24343fb4e5b997cb2ef390b6640dd8d941ee5d
user <user@users-MacBook-Pro.local>
2023-06-20 13:06:30 -0700

    created function missingNumbers. This function will take a parameter of a number array and sort that array in order and then return a new array of the numbers that are missing between each number

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 // Question 1a
-let ages = [3,9,23,64,2,8,28,93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 console.log(ages[0] - ages[ages.length - 1]);
 // 1b
 ages.push(9);
 console.log(ages[0] - ages[ages.length - 1]);
 // 1c
 let ageAverage = 0;
-for(let i=0; i<ages.length; i++) {
+for (let i = 0; i < ages.length; i++) {
   ageAverage += ages[i];
 }
 
-console.log(Math.round(ageAverage/ages.length));
+console.log(Math.round(ageAverage / ages.length));
 
 // Question 2a
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 let averageNameValue = 0;
 
-  for(let i=0; i<names.length; i++){
-    averageNameValue += names[i].length;
-  }
+for (let i = 0; i < names.length; i++) {
+  averageNameValue += names[i].length;
+}
 
 console.log(Math.round(averageNameValue / names.length));
 
 // 2b
 let strOfNames = '';
-  for(let i=0; i<names.length; i++){
-    strOfNames += `${names[i]} `;
-  }
+for (let i = 0; i < names.length; i++) {
+  strOfNames += `${names[i]} `;
+}
 
 console.log(strOfNames);
 
@@ -43,16 +43,16 @@ console.log(strOfNames);
 // Question 5
 let nameLengths = [];
 
-  for(let i=0; i<names.length; i++) {
-    nameLengths.push(names[i].length)
-  }
+for (let i = 0; i < names.length; i++) {
+  nameLengths.push(names[i].length)
+}
 
 console.log(nameLengths);
 
 // Question 6
 let sum = 0;
 
-for(let i=0; i<nameLengths.length; i++) {
+for (let i = 0; i < nameLengths.length; i++) {
   sum += nameLengths[i];
 }
 
@@ -61,7 +61,7 @@ console.log(sum);
 // Question 7
 function concatItself(word, n) {
   let str = '';
-  for(let i=0; i <n; i++) {
+  for (let i = 0; i < n; i++) {
     str += word;
   }
   return str;
@@ -76,12 +76,12 @@ console.log(fullName('John', 'Smith'));
 
 // Question 9
 
-let testArrayOne = [10,10,10,25]; //sum 55
-let testArrayTwo = [2,10,65,14,1,23,51]; // sum 166
-let testArrayThree = [15,15,60,10]; // sum 100
-let testArrayFour = [20,10,5]; // sum 35
+let testArrayOne = [10, 10, 10, 25]; //sum 55
+let testArrayTwo = [2, 10, 65, 14, 1, 23, 51]; // sum 166
+let testArrayThree = [15, 15, 60, 10]; // sum 100
+let testArrayFour = [20, 10, 5]; // sum 35
 
-let sumOfArray = (arr1) => arr1.reduce((a,b)=> a + b) > 100;
+let sumOfArray = (arr1) => arr1.reduce((a, b) => a + b) > 100;
 
 console.log(sumOfArray(testArrayOne)); //false
 console.log(sumOfArray(testArrayTwo)); //true
@@ -90,7 +90,7 @@ console.log(sumOfArray(testArrayThree));// false
 // Question 10
 function averageNumArr(arr1) {
   let average = 0;
-  for(let i=0; i<arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     average += arr1[i];
   }
   return average / arr1.length;
@@ -99,11 +99,11 @@ function averageNumArr(arr1) {
 console.log(averageNumArr(testArrayFour));
 
 // Question 11
-function arraysOverUnder(arr1,arr2) {
-  return (arr1.reduce((a,b)=> a + b) > arr2.reduce((a,b)=> a + b));
+function arraysOverUnder(arr1, arr2) {
+  return (arr1.reduce((a, b) => a + b) > arr2.reduce((a, b) => a + b));
 }
 
-console.log(arraysOverUnder(testArrayOne,testArrayTwo)); // false
+console.log(arraysOverUnder(testArrayOne, testArrayTwo)); // false
 console.log(arraysOverUnder(testArrayThree, testArrayFour)) // true
 
 // Question 12
@@ -118,3 +118,18 @@ console.log(willBuyDrink(false, 15)); // false
 
 // Question 13
 
+function missingNumbers(arr1) {
+  arr1.sort((a, b) => a - b);
+  let missingNums = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] < arr1[i + 1] - 1) {
+      let difference = (arr1[i + 1] - arr1[i]) - 1;
+      for (let j = 1; j <= difference; j++) {
+          missingNums.push(arr1[i] + j);
+      }
+    }
+  }
+  return missingNums;
+}
+
+console.log(missingNumbers(testArrayTwo))
